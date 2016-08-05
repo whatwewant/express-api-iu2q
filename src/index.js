98a87bf95cf07347312aa5077e9038da3e85904b
eason <uniquecolesmith@gmail.com>
2016-08-05 12:42:39 +0800

    + ignore default config

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ var router = require('./route');
 
 module.exports = function (conf) {
   assert.ok(typeof conf === 'object', 'Require Object: { ACCESS_KEY, SECRET_KEY, BUCKET, DOMAIN }');
+
   assert.ok(conf.hasOwnProperty('ACCESS_KEY'), 'Require Object: { ACCESS_KEY, SECRET_KEY, BUCKET, DOMAIN }');
   assert.ok(conf.hasOwnProperty('SECRET_KEY'), 'Require Object: { ACCESS_KEY, SECRET_KEY, BUCKET, DOMAIN }');
   assert.ok(conf.hasOwnProperty('BUCKET'), 'Require Object: { ACCESS_KEY, SECRET_KEY, BUCKET, DOMAIN }');
   assert.ok(conf.hasOwnProperty('DOMAIN'), 'Require Object: { ACCESS_KEY, SECRET_KEY, BUCKET, DOMAIN }');
+
+  assert.ok(conf.ACCESS_KEY !== '<YOUR ACCESS KEY>', 'Require Object: { ACCESS_KEY, SECRET_KEY, BUCKET, DOMAIN }');
+  assert.ok(conf.SECRET_KEY !== '<YOUR SECRET KEY>', 'Require Object: { ACCESS_KEY, SECRET_KEY, BUCKET, DOMAIN }');
+  assert.ok(conf.BUCKET !== '<YOUR BUCKET NAME>', 'Require Object: { ACCESS_KEY, SECRET_KEY, BUCKET, DOMAIN }');
+  assert.ok(conf.DOMAIN !== '<YOUR DOMAIN>', 'Require Object: { ACCESS_KEY, SECRET_KEY, BUCKET, DOMAIN }');
 
   return router(conf);
 };
